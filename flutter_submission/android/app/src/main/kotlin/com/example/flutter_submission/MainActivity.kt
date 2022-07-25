98cec8908fd16a9e234f879c5d0b2c560e0e636b
Dimas pradipta <dimaspradipta19@gmail.com>
2022-07-25 15:31:12 +0700

    submission

--- flutter_submission/android/app/src/main/kotlin/com/example/flutter_submission/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_submission
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_submission/lib/utils/theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color redColor = Color(0xFFEF3024);
-const Color blackColor = Color(0xFF3E3E3E);
-const Color greyColor = Color(0xFF7B7B7B);
-const Color backgroundColor = Color(0xFFF3F3F9);
-const Color whiteColor = Color(0xFFD2D2D2);
-const Color whiteColor2 = Color(0xFFFFFFFF);
